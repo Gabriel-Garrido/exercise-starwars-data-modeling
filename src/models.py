95b334678280fccce623077d9dfe527c8bd9f0c4
Gabriel-Garrido <gabrielgarrido89@gmail.com>
2022-12-16 01:32:15 +0000

    agrego modelos con relaciones

--- src/models.py
+++ src/models.py
@@ -8,26 +8,67 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+# Cada usuario puede tener un personaje favorito y una nave favorita.
+
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    first_name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    favorites_starship = Column(Integer, ForeignKey('starship.id'))
+    favorite_character = Column(Integer, ForeignKey('character.id'))
 
     def to_dict(self):
         return {}
+
+
+# Cada personaje puede tener varias naves y pertenecer a un planeta
+
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    age = Column(Integer, nullable=False)
+    planet = Column(Integer, ForeignKey('planets.id'))
+    starships = relationship('Starship')
+    users = relationship('User')
+
+    def to_dict(self):
+        return {}
+
+
+# Cada nave pertenece a un personaje
+
+class Starship(Base):
+    __tablename__ = 'starship'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250))
+    size = Column(Integer, nullable=False)
+    max_speed = Column(Integer, nullable=False)
+    acceleration = Column(Integer, nullable=False)
+    users = relationship('User')
+    owner = Column(Integer, ForeignKey('character.id'))
+
+    def to_dict(self):
+        return {}
+
+
+# Cada planeta puede tener varios personajes
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250), nullable=False)
+    size = Column(Integer, nullable=False)
+    population = relationship('Character')
+
+    def to_dict(self):
+        return {}
+
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
